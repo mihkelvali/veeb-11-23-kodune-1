d50ba8c3c2ee7171692cf5fa3b76996d00b54da3
mihkelvali <mihkel.vali@hotmail.com>
2023-11-08 18:38:02 +0200

    kodune 1

---
+++ src/RaamatuDetail.js
@@ -0,0 +1,15 @@
+function RaamatuDetail() {
+    return (
+        <div style={{ textAlign: 'left', maxWidth: 400 }}>
+            <h2>1984</h2>
+            <div>George Orwell</div>
+            <div>
+                <p>Winston Smith toes the Party line, rewriting history to satisfy the demands of the Ministry of Truth. With each lie he writes, Winston grows to hate the Party that seeks power for its own sake and persecutes those who dare to commit thoughtcrimes. But as he starts to think for himself, Winston can’t escape the fact that Big Brother is always watching...</p>
+                <p>A startling and haunting novel, 1984 creates an imaginary world that is completely convincing from start to finish. No one can deny the novel’s hold on the imaginations of whole generations, or the power of its admonitions—a power that seems to grow, not lessen, with the passage of time.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default RaamatuDetail;
